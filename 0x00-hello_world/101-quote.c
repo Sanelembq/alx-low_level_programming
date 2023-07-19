e30365d61e7f74bc1a9e9f01c0e316571b60c6b3
sanele <kayceembq@gmail.com>
2023-07-19 16:10:48 -0700

    qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+
+/**
+ * main - prints "and that pieceof art is usefull" - Dora Korpar, 2015-10-19
+ * followed by a newline, to standard error
+ * Return: Alway 1.
+ */
+
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19", 59);
+	return (1);
+}
